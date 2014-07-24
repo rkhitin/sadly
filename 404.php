35c40b59824ae3965c0fdc8ed5cd13b21a8c0ee7
ron <ron@ron-dev@example.org>
2014-07-24 09:10:27 +0400

    add 404-2

--- 404.php
+++ 404.php
@@ -1,6 +1,9 @@
 <?php get_header(); ?>
 
-<img src="<?=get_template_directory_uri()?>/img/404.png" alt="">
+    <!-- Fixed navbar -->
+    <?php get_template_part('nav'); ?>
+
+    <img src="<?=get_template_directory_uri()?>/img/404.png" alt="">
 
 <?php get_footer(); ?>
 
