fe502103f353e31f3289d4cc0d3028c856aef7b4
ron <ron@ron-dev@example.org>
2014-07-24 09:16:58 +0400

    fix 404 img2

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@
     <?php get_template_part('nav'); ?>
 
     <div class="text-center">
-        <img src="<?=get_template_directory_uri()?>/img/404.png" alt="">
+        <img id='img404' src="<?=get_template_directory_uri()?>/img/404.png" alt="">
     </div>
 
 <?php get_footer(); ?>
