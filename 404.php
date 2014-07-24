ed580b000b39c991f1fba7bbfd77e5e0b0e7fb55
ron <ron@ron-dev@example.org>
2014-07-24 09:11:57 +0400

    add 404-3

--- 404.php
+++ 404.php
@@ -3,7 +3,9 @@
     <!-- Fixed navbar -->
     <?php get_template_part('nav'); ?>
 
-    <img src="<?=get_template_directory_uri()?>/img/404.png" alt="">
+    <div class="text-center">
+        <img src="<?=get_template_directory_uri()?>/img/404.png" alt="">
+    </div>
 
 <?php get_footer(); ?>
 
